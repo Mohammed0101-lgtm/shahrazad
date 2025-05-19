d9188e8ec6fa389384437e9720fd6ec1c4642858
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-19 18:07:07 +0100

    stuff

--- src/eval.h
+++ src/eval.h
@@ -4,20 +4,23 @@
 #include "position.h"
 #include <cassert>
 
-int simple_evaluate(const Position& pos) {
-    uint8_t color = pos.getSide();
+namespace Shahrazad {
+namespace eval {
+
+int simple_evaluate(const position::Position& pos) {
+    types::Color color = pos.getSide();
     int     eval  = 0;
 
-    eval += pos.numberOf(PAWN, color) * PAWN_VAL;
-    eval += pos.numberOf(KNIGHT, color) * KNIGHT_VAL;
-    eval += pos.numberOf(BISHOP, color) * BISHOP_VAL;
-    eval += pos.numberOf(ROOK, color) * ROOK_VAL;
-    eval += pos.numberOf(QUEEN, color) * QUEEN_VAL;
+    eval += pos.numberOf(types::PieceType::PAWN, color) * types::PAWN_VAL;
+    eval += pos.numberOf(types::PieceType::KNIGHT, color) * types::KNIGHT_VAL;
+    eval += pos.numberOf(types::PieceType::BISHOP, color) * types::BISHOP_VAL;
+    eval += pos.numberOf(types::PieceType::ROOK, color) * types::ROOK_VAL;
+    eval += pos.numberOf(types::PieceType::QUEEN, color) * types::QUEEN_VAL;
 
     return eval;
 }
 
-int network_eval(const Position& pos, NNue network, NNue::Accumulator<size>& caches) {
+int network_eval(const position::Position& pos, nnue::NNue network, nnue::NNue::Accumulator<nnue::size>& caches) {
     assert(!pos.inCheck);
 
     bool use_smallnet = false;
@@ -30,3 +33,6 @@ int network_eval(const Position& pos, NNue network, NNue::Accumulator<size>& cac
 
     return simple_eval;
 }
+
+}  // namespace eval
+}  // namespace Shahrazad
