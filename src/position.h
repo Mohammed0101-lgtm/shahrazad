2ba987f044185c3f47e90fda60baa3405295ffa2
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-19 18:09:04 +0100

    stuff

--- src/position.h
+++ src/position.h
@@ -6,32 +6,36 @@
 #include <vector>
 
 
+namespace Shahrazad {
+namespace position {
+
 inline uint64_t pieceKeys[12][64];
 inline uint64_t sideKey[2];
 
+
 class Position {
    public:
-    Bitboard white_pawns;
-    Bitboard white_king;
-    Bitboard white_knights;
-    Bitboard white_bishops;
-    Bitboard white_queens;
-    Bitboard white_rooks;
+    board::Bitboard white_pawns;
+    board::Bitboard white_king;
+    board::Bitboard white_knights;
+    board::Bitboard white_bishops;
+    board::Bitboard white_queens;
+    board::Bitboard white_rooks;
 
-    Bitboard black_pawns;
-    Bitboard black_king;
-    Bitboard black_knights;
-    Bitboard black_bishops;
-    Bitboard black_queens;
-    Bitboard black_rooks;
+    board::Bitboard black_pawns;
+    board::Bitboard black_king;
+    board::Bitboard black_knights;
+    board::Bitboard black_bishops;
+    board::Bitboard black_queens;
+    board::Bitboard black_rooks;
 
-    uint8_t   current_side;
-    uint8_t   enPassant_square    = NOPE;
+    types::Color   current_side;
+    types::Square   enPassant_square    = types::Square::NONE;
     uint8_t   stacked_his         = 0;
     uint8_t   half_moves          = 0;
     uint8_t   fifty_moves_counter = 0;
     uint8_t   ply_fromNull        = 0;
-    pieceType pieces[64];
+    types::PieceType pieces[64];
     bool      attacked_white[64];
     bool      attacked_black[64];
     bool      castle_perm[2];
@@ -44,60 +48,40 @@ class Position {
 
 
     Position() :
-        current_side(WHITE) {
-        for (int i = 0; i < 64; i++)
-            attacked_black[i] = false;
+        current_side(types::Color::WHITE),
+        attacked_white{false},
+        attacked_black{false} {}
 
-        for (int i = 0; i < 64; i++)
-            attacked_white[i] = false;
-    }
-
-    Position(int color) :
-        current_side(color) {
-        for (int i = 0; i < 64; i++)
-            attacked_black[i] = false;
-
-        for (int i = 0; i < 64; i++)
-            attacked_white[i] = false;
-    }
-
+    Position(types::Color color) :
+        current_side(color),
+        attacked_black{false},
+        attacked_white{false} {}
+    
+    Position(const std::vector<uint64_t>& played_positions) {}
+    
     Position get_previous_position() const;
-
-    Bitboard _black_occupancy() const;
-
-    Bitboard _white_occupancy() const;
-
-    Bitboard occupancy() const;
-
+    board::Bitboard _black_occupancy() const;
+    board::Bitboard _white_occupancy() const;
+    board::Bitboard occupancy() const;
     void reset();
-
     void switch_side();
-
     void undo_move();
-
     void make_null_move();
-
     void take_null_move();
-
     bool canCastle(int castle_side) const;
-
-    bool isAttacked(int sq, int color) const;
-
-    bool isPiece(int sq, pieceType piece) const;
-
-    pieceType pieceOn(const int sq) const;
-
-    uint8_t getSide() const;
-
-    uint8_t isPinned(const int sq) const;
-
-    uint8_t material_score() const;
-
-    uint8_t piece_count() const;
-
-    uint8_t numberOf(pieceType piece, int color) const;
-
-    uint8_t king_square(int color) const;
-
-    uint8_t getColor(int sq) const;
+    bool isAttacked(types::Square sq, types::Color color) const;
+    bool isPiece(int sq, types::PieceType piece) const;
+    types::PieceType pieceOn(const int sq) const;
+    types::PieceType pieceOn(const types::Square sq) const;
+    types::Color getSide() const;
+    types::Square isPinned(const types::Square sq) const;
+    unsigned int material_score() const;
+    unsigned int piece_count() const;
+    unsigned int numberOf(types::PieceType piece, types::Color color) const;
+    types::Square king_square(types::Color color) const;
+    types::Color getColor(types::Square sq) const;
 };
+
+
+}  // namespace position
+}  // namespace Shahrazad
