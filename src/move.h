96f1cd79fc0f740bc5ac419c61ebf31c71b4eb2e
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-19 18:07:43 +0100

    stuff

--- src/move.h
+++ src/move.h
@@ -4,63 +4,47 @@
 #include "position.h"
 #include "types.h"
 
-typedef enum {
-    Quiet,
-    KSCastle,
-    QSCastle,
-    Capture,
-    enPassant,
-    Promo,
-    NOMOVE
-} moveType;
+namespace Shahrazad {
+namespace movegen {
+
 
 struct ScoredMove {
-    Move move;
-    int  score = 0;
+    types::Move move;
+    int         score = 0;
 };
 
 struct MoveList {
     ScoredMove moves[1024];
     int        size = 0;
 
-    void append(const Move& _move) {
+    void append(const types::Move& _move) {
         ScoredMove sc_move;
         sc_move.move = _move;
 
         if (sizeof(moves) >= static_cast<unsigned long>(size))
-            return;
+          {  return;}
 
         moves[size++] = sc_move;
     }
 };
 
-enum PickerType : uint8_t {
-    SEARCH,
-    QSEARCH
-};
+enum PickerType : uint8_t { SEARCH, QSEARCH };
 
-std::vector<uint8_t> king_moves(const uint8_t square, uint8_t color, const Position& pos);
+void                     do_move(position::Position& pos, const types::Move& move);
+std::vector<types::Move> king_moves(const types::Square square, types::Color color, const position::Position& pos);
+std::vector<types::Move> pawn_moves(const types::Square square, types::Color color, const position::Position& pos);
+std::vector<types::Move> rook_moves(const types::Square square, types::Color color, const position::Position& pos);
+std::vector<types::Move> knight_moves(const types::Square square, types::Color color, const position::Position& pos);
+std::vector<types::Move> bishop_moves(const types::Square square, types::Color color, const position::Position& pos);
+std::vector<types::Move> queen_moves(const types::Square square, const types::Color color,
+                                     const position::Position& pos);
+std::vector<types::Move> generate_all_moves(const position::Position& pos, types::Color color);
+std::vector<types::Square> piece_squares(types::PieceType piece_type, const position::Position& pos);
+bool                     isPseudoLegal(const position::Position& pos, const types::Move& move);
+bool                     isLegal(const position::Position& pos, const types::Move& move);
+bool                     is_tactical(const types::Move& _move);
+void                     setAttackedSquares(position::Position& pos);
+board::Bitboard          get_piece_attacks(const position::Position& pos, types::Square square);
 
-std::vector<uint8_t> pawn_moves(const uint8_t square, uint8_t color, const Position& pos);
-
-std::vector<uint8_t> rook_moves(const uint8_t square, uint8_t color, const Position& pos);
-
-std::vector<uint8_t> knight_moves(const uint8_t square, uint8_t color, const Position& pos);
-
-std::vector<uint8_t> bishop_moves(const uint8_t square, uint8_t color, const Position& pos);
-
-std::vector<uint8_t> queen_moves(const uint8_t square, const uint8_t color, const Position& pos);
-
-std::vector<Move> generate_all_moves(const Position& pos, uint8_t color);
-
-std::vector<uint8_t> piece_squares(pieceType piece_type, const Position& pos);
-
-bool isPseudoLegal(const Position& pos, const Move& move);
-
-bool isLegal(const Position& pos, const Move& move);
-
-bool is_tactical(const Move& _move);
-
-void setAttackedSquares(Position& pos);
-
-Bitboard get_piece_attacks(const Position& pos, uint8_t square);
+}  // namespace movegen
+}  // namespace Shahrazad
