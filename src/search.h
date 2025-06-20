8c4ed03fde29ff99287b5eff48b4a93043aae86e
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-20 20:21:17 +0100

    stuff

--- src/search.h
+++ src/search.h
@@ -5,6 +5,10 @@
 #include "move.h"
 #include "position.h"
 
+
+namespace Shahrazad {
+namespace search {
+
 // global constants (I hate the word global but that's life)
 inline constexpr uint8_t MAXSCORE      = 32670;
 inline constexpr uint8_t MAX_DEPTH     = 256;
@@ -15,12 +19,6 @@ constexpr uint8_t        SCORE_NONE    = 32001;
 constexpr uint8_t        MATE_FOUND    = 31744;
 constexpr uint8_t        CH_MAX        = 16384;
 
-// time spent by the thread (Life is short)
-inline uint64_t GetTimeMs() {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::steady_clock::now().time_since_epoch())
-      .count();
-}
 
 // keep data about the current thread
 struct SearchInfo {
@@ -50,83 +48,35 @@ struct SearchData {
 
 // principle variation table
 struct PvTable {
-    uint8_t pvLength[MAXDEPTH + 1];
-    Move    pvArray[MAXDEPTH + 1][MAXDEPTH + 1];
-};
-
-// data held in the thread about the position state
-struct ThreadData {
-    Position   pos;
-    PvTable    pvTable;
-    uint64_t   nodeSpentTable[64 * 64];
-    uint8_t    id = 0;
-    uint8_t    rootDepth;
-    uint8_t    nmpPlies;
-    SearchData search_data;
-    SearchInfo info;
+    uint8_t     pvLength[MAXDEPTH + 1];
+    types::Move pvArray[MAXDEPTH + 1][MAXDEPTH + 1];
 };
 
 // self explanatory
 struct SearchStack {
-    int16_t staticEval;
-    Move    excludedMove;
-    Move    move;
-    Move    searchKiller;
-    uint8_t ply;
-    uint8_t doubleExtensions;
+    int16_t     staticEval;
+    types::Move excludedMove;
+    types::Move move;
+    types::Move searchKiller;
+    uint8_t     ply;
+    uint8_t     doubleExtensions;
     uint8_t (*contHistEntry)[12 * 64];
 };
 
 // the current search node of the tree
 struct SearchNode {
-    SearchNode* parent;
-    Position    pos;
-    Position**  children;
-    uint8_t     val;
+    SearchNode*          parent;
+    position::Position   pos;
+    position::Position** children;
+    uint8_t              val;
 };
 
-// done with this node
-bool NodesOver(const SearchInfo* info) { return info->nodeset && info->nodes > info->nodeslimit; }
-
-// thread ran out of time
-bool TimeOver(const SearchInfo* info) {
-    return NodesOver(info)
-        || ((info->timeset || info->movetimeset) && ((info->nodes & 1023) == 1023)
-            && GetTimeMs() > info->stoptimeMax);
-}
 
 // searching or just chilling
-enum state {
-    idle,
-    search_state
+enum class state : int {
+    IDLE,
+    SEARCH_STATE
 };
 
-// threads allocated
-inline std::vector<std::thread> threads;
-inline std::vector<ThreadData>  threads_data;
-
-// get node's bitboard
-inline uint64_t get_nodes() {
-    uint64_t nodes = 0ULL;
-    for (auto& t : threads_data)
-        nodes += t.info.nodes;
-
-    return nodes;
-}
-
-// stop the thread for whatever reason
-inline void thread_interrupt() {
-    for (auto& t : threads_data)
-        t.info.stopped = true;
-
-    for (auto& t : threads)
-    {
-        if (t.joinable())
-            t.join();
-    }
-
-    threads.clear();
-}
-
-/*-- note that yes these thread things should be in a seperate file but i did put them here temporarily to solve a circle include problem
- * --*/
+}  // namespace search
+}  // namespace Shahrazad
