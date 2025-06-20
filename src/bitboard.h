4a7eb9dc09a3f0f2e52f768dce9a6f3c4c66f1dd
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-20 20:22:02 +0100

    stuff

--- src/bitboard.h
+++ src/bitboard.h
@@ -2,6 +2,10 @@
 
 #include "types.h"
 
+
+namespace Shahrazad {
+namespace board {
+
 class Bitboard {
    protected:
     uint64_t bit_board;
@@ -13,20 +17,16 @@ class Bitboard {
         bit_board(val) {}
 
     uint64_t board() const;
-
-    uint8_t square() const;
-
-    bool is_bitset(const uint8_t& pos) const;
-
-    void flip_bit(const uint8_t& pos);
-
-    void set_bit(const uint8_t& pos);
-
-    void clear_bit(const uint8_t& pos);
-
-    void move_bit(const uint8_t& start_pos, const uint8_t& end_pos);
-
-    void print() const;
-
-    uint8_t count() const;
+    uint8_t  square() const;
+    bool     is_bitset(const uint8_t& pos) const;
+    bool     is_bitset(const types::Square& pos) const;
+    void     flip_bit(const uint8_t& pos);
+    void     set_bit(const uint8_t& pos);
+    void     clear_bit(const uint8_t& pos);
+    void     move_bit(const uint8_t& start_pos, const uint8_t& end_pos);
+    void     print() const;
+    uint8_t  count() const;
 };
+
+}  // namespace Shahrazad
+}  // namespace board
